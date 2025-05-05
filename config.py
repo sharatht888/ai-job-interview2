4da3be9e983bfb8955e340efa650814b36bf9864
sharatht888 <113619551+sharatht888@users.noreply.github.com>
2025-05-05 20:29:02 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 SECRET_KEY = "admin"
 
-DB_DRIVER = '{ODBC Driver 17 for SQL Server}'  # Or 18 if you're using it
+DB_DRIVER = '{ODBC Driver 18 for SQL Server}'  # Or 18 if you're using it
 DB_SERVER = 'aijobinterview.database.windows.net'
 DB_NAME = 'interview_system'
 DB_USER = 'sqldbadmin'
